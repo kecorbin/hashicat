85d03dfb1e29ca834f7d9261ea6b4241fede9593
Kevin Corbin <kcorbin@hashicorp.com>
2019-10-03 09:11:50 -0500

    add arcade

---
+++ arcade.tf
@@ -0,0 +1,12 @@
+module "web_app_container" {
+  source              = "app.terraform.io/kecorbin/web-app-container/azurerm"
+  name                = "${var.prefix}"
+  port                = "80"
+  https_only          = "false"
+  resource_group_name = "${azurerm_resource_group.myresourcegroup.name}"
+  container_type      = "docker"
+  container_image     = "scarolan/palacearcade"
+}
+output "container_app_url" {
+  value = "http://${module.web_app_container.hostname}"
+}
