466c3e2477763a1c9a076b9675770aa18504321b
Kevin Corbin <kcorbin@hashicorp.com>
2019-10-10 08:55:57 -0500

    refactor

--- palace_arcade.tf
+++
@@ -1,9 +0,0 @@
-module "web_app_container" {
-  source              = "app.terraform.io/kcorbin/web-app-container/azurerm"
-  name                = "${var.prefix}"
-  port                = "80"
-  https_only          = "false"
-  resource_group_name = azurerm_resource_group.myresourcegroup.name
-  container_type      = "docker"
-  container_image     = "scarolan/palacearcade"
-}
